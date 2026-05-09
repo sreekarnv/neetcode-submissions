51b271c87cbd5c59d3fcb81f347812a34d3eab00
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2026-05-09 15:57:54 +0530

    Add: two-integer-sum - submission-17

---
+++ Data Structures & Algorithms/two-integer-sum/submission-17.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mapper = {}
+
+        for i, n in enumerate(nums):
+            m = target - n
+            
+            if m in mapper:
+                return [mapper[m], i]
+            
+            mapper[n] = i
+
+        return []
