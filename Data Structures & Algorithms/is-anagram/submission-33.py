1a6f3dd61757116a8f482fe8b9ea1884bf6fdd97
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2026-05-25 11:48:39 +0530

    Add: is-anagram - submission-33

---
+++ Data Structures & Algorithms/is-anagram/submission-33.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if not len(s) == len(t):
+            return False
+
+        sl = [0] * 26
+        tl = [0] * 26
+
+        for i in range(len(s)):
+            sl[ord(s[i]) - ord('a')] += 1
+            tl[ord(t[i]) - ord('a')] += 1
+        
+        return sl == tl
