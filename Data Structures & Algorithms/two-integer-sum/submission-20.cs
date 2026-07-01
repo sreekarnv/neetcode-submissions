690e448ba11f149ab5b52ad9702a6c064a9c889c
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2026-07-01 14:17:33 +0530

    Add: two-integer-sum - submission-20

---
+++ Data Structures & Algorithms/two-integer-sum/submission-20.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] result = {-1, -1};
+        Dictionary<int, int> mapper = new Dictionary<int, int> {};
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int m = target - nums[i];
+            if (mapper.ContainsKey(m))
+            {
+                result[0] = mapper[m];
+                result[1] = i;
+                break;
+            }
+
+            mapper.Add(nums[i], i);
+        }
+
+        return result;
+    }
+}
