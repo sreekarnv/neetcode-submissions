96c5044f406bfd955162aa940353cde101495879
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-03 17:28:41 +0530

    Update: valid-palindrome-ii - submission-0

--- Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -1,8 +1,8 @@
 class Solution:
-    def isPalindrome(self, s: str, l: int = 0, r: int = -1) -> bool:
-        if r == -1:
+    def isPalindrome(self, s: str, l: int = 0, r: int | None = None) -> bool:
+        if r == None:
             r = len(s) - 1
-
+        
         while l < r:
             if not s[l] == s[r]: return False
 
@@ -18,8 +18,8 @@ class Solution:
         while l < r:
             if not s[l] == s[r]:
                 return self.isPalindrome(s, l + 1, r) or self.isPalindrome(s, l, r - 1)
-
+            
             l += 1
             r -= 1
 
-        return True 
+        return True
