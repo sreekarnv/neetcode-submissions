476435ae937659035dcaf3833480826e56f6c1c4
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-03 22:00:39 +0530

    Update: permutation-string - submission-0

--- Data Structures & Algorithms/permutation-string/submission-0.py
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -1,22 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        ch1_mapper = {}
-        for ch1 in s1:
-            ch1_mapper[ch1] = 1 + ch1_mapper.get(ch1, 0)
+        s1m = {}
+        for ch in s1:
+            s1m[ch] = 1 + s1m.get(ch, 0)
         
+        s2m = {}
         l = 0
-        ch2_mapper = {}
-        for r, ch2 in enumerate(s2):
-            ch2_mapper[ch2] = 1 + ch2_mapper.get(ch2, 0)
+        for r, ch in enumerate(s2):
+            s2m[ch] = 1 + s2m.get(ch, 0)
 
-            while r - l + 1 > len(s1):
-                ch2_mapper[s2[l]] -= 1
+            if r - l + 1 > len(s1):
+                s2m[s2[l]] -= 1
 
-                if ch2_mapper[s2[l]] == 0:
-                    del ch2_mapper[s2[l]]
-
+                if s2m[s2[l]] == 0:
+                    del s2m[s2[l]]
+                
                 l += 1
-
-            if ch1_mapper == ch2_mapper: return True
+            
+            if s1m == s2m: return True
         
-        return ch1_mapper == ch2_mapper
+        return False
