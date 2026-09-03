0c1ce8c38150951a1e2cdceee96f1e528acdea5d
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-03 18:08:50 +0530

    Update: 4sum - submission-0

--- Data Structures & Algorithms/4sum/submission-0.py
+++ Data Structures & Algorithms/4sum/submission-0.py
@@ -1,32 +1,34 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
+
         results = []
 
-        for j, m in enumerate(nums):
-            if j > 0 and m == nums[j - 1]:
-                continue
-            
-            for i in range(j + 1, len(nums)):
-                if i > j + 1 and nums[i] == nums[i - 1]:
-                    continue
-                
-                l = i + 1
+        for i in range(len(nums)):
+            if i > 0 and nums[i - 1] == nums[i]: continue
+
+            for j in range(i + 1, len(nums)):
+                if j > i + 1 and nums[j] == nums[j - 1]: continue
+
+                l = j + 1
                 r = len(nums) - 1
 
                 while l < r:
-                    curr = nums[j] + nums[i] + nums[l] + nums[r]
-                    if curr > target: r -= 1
-                    elif curr < target: l += 1
+                    curr = nums[i] + nums[j] + nums[l] + nums[r]
+
+                    if curr > target:
+                        r -= 1
+                    elif curr < target:
+                        l += 1
                     else:
-                        results.append((nums[j], nums[i], nums[l], nums[r]))
+                        results.append([
+                            nums[i], nums[j], nums[l], nums[r]
+                        ])
                         l += 1
                         r -= 1
 
-                        while l < r and nums[l] == nums[l - 1]:
-                            l += 1
+                        while l < r and nums[l] == nums[l - 1]: l += 1
 
-                        while l < r and nums[r] == nums[r + 1]:
-                            r -= 1
-                
+                        while l < r and nums[r] == nums[r + 1]: r -= 1
+        
         return results
