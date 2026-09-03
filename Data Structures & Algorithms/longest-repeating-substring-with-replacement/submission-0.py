1decca8a12f521e8d3a0a29bbc7b6c97e4b3ce48
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-03 21:53:31 +0530

    Update: longest-repeating-substring-with-replacement - submission-0

--- Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -1,22 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        mapper = {}
         longest = 0
-        freq = {}
-        max_freq = 0
-
+        freq = 0
         l = 0
-        for r, ch in enumerate(s):
-            freq[ch] = 1 + freq.get(ch, 0)
-            max_freq = max(freq[ch], max_freq)
 
-            if r - l + 1 - max_freq > k:
-                freq[s[l]] -= 1
-                
-                if freq[s[l]] == 0:
-                    del freq[s[l]]
-                
+        for r, ch in enumerate(s):
+            mapper[ch] = 1 + mapper.get(ch, 0)
+            freq = max(freq, mapper[ch])
+            
+            if r - l + 1 - freq > k:
+                mapper[s[l]] -= 1
                 l += 1
 
-            longest = max(r - l + 1, longest)
+            longest = max(longest, r - l + 1)
 
-        return longest
+        return longest 
