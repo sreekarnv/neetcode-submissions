bb3115a7d1dd3b3f2a8d386ed0db1fccae58272d
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-03 17:32:32 +0530

    Update: merge-sorted-array - submission-0

--- Data Structures & Algorithms/merge-sorted-array/submission-0.py
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.py
@@ -5,6 +5,7 @@ class Solution:
         """
         i = m - 1
         j = n - 1
+
         k = len(nums1) - 1
 
         while i >= 0 and j >= 0:
@@ -14,8 +15,10 @@ class Solution:
             else:
                 nums1[k] = nums2[j]
                 j -= 1
+            
             k -= 1
-
+        
         while j >= 0:
             nums1[j] = nums2[j]
             j -= 1
+        
