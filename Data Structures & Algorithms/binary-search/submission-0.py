02f4cd4eac1bce63a3b2c12f47d31cfe085c5902
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-04 18:45:06 +0530

    Update: binary-search - submission-0

--- Data Structures & Algorithms/binary-search/submission-0.py
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -9,8 +9,8 @@ class Solution:
             if nums[mid] == target: return mid
 
             if nums[mid] > target:
-                r -= 1
+                r = mid - 1
             else:
-                l += 1
+                l = mid + 1
         
         return -1
