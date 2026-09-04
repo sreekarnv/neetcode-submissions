b1f389ba782d147651d77896d9802104f965fed9
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-04 19:19:45 +0530

    Update: merge-two-sorted-linked-lists - submission-0

--- Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.py
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.py
@@ -10,12 +10,12 @@ class Solution:
         tail = d
 
         while list1 and list2:
-            if list1.val > list2.val:
-                tail.next = list2
-                list2 = list2.next
-            else:
+            if list1.val < list2.val:
                 tail.next = list1
                 list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
             
             tail = tail.next
         
