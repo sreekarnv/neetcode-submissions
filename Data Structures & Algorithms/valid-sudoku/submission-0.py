7097908181e93a654247e2790bcc2476ca62c8b8
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-14 05:25:54 +0530

    Update: valid-sudoku - submission-0

--- Data Structures & Algorithms/valid-sudoku/submission-0.py
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -1,23 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        boxes = defaultdict(set)
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        boxes = collections.defaultdict(set)
 
-        for row in range(9):
-            for col in range(9):
+        for row in range(len(board)):
+            for col in range(len(board[0])):
 
                 if board[row][col] == ".":
                     continue
                 
-                curr = board[row][col]
-                boxId = (row // 3, col // 3)
+                current = board[row][col]
+                rc = (row // 3, col // 3)
 
-                if curr in rows[row] or curr in cols[col] or curr in boxes[boxId]:
+                if current in rows[row] or current in cols[col] or current in boxes[rc]:
                     return False
                 
-                rows[row].add(curr)
-                cols[col].add(curr)
-                boxes[boxId].add(curr)
-        
+                rows[row].add(current)
+                cols[col].add(current)
+                boxes[rc].add(current)
+
         return True
