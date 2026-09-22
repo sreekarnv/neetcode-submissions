18cea22392e7d1a4ab4963b0f77ca9047606db18
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-09-22 17:07:20 +0530

    Update: reverse-a-linked-list - submission-0

--- Data Structures & Algorithms/reverse-a-linked-list/submission-0.py
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.py
@@ -6,7 +6,7 @@
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
+        curr, prev = head, None
 
         while curr:
             tmp = curr.next
